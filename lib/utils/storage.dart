25fca80e3423c673db59c4c45c06cb7ec6977206
TechieChuks <chuksanoliefo@gmail.com>
2025-09-29 20:29:43 +0100

    Add Storage Utility – Implement lib/utils/storage.dart for reading/writing users.json

---
+++ lib/utils/storage.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:dart_cli_full_banking_system/models/user.dart';
+
+class Storage {
+  final String usersFile = 'data/users.json';
+
+  Future<List<User>> readUsers() async {
+    final file = File(usersFile);
+    if (!await file.exists()) {
+      await file.create(recursive: true);
+      await file.writeAsString('[]');
+    }
+    final content = await file.readAsString();
+    final List<dynamic> jsonList = jsonDecode(content);
+    return jsonList.map((u) => User.fromJson(u)).toList();
+  }
+
+  Future<void> saveUsers(List<User> users) async {
+    final file = File(usersFile);
+    await file.writeAsString(jsonEncode(users.map((u) => u.toJson()).toList()));
+  }
+}
