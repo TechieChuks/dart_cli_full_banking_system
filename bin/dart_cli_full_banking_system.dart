3cf353cb37ad8685a03302d3e5d75c1840949dd1
TechieChuks <chuksanoliefo@gmail.com>
2025-09-29 19:54:26 +0100

    project created

---
+++ bin/dart_cli_full_banking_system.dart
@@ -0,0 +1,5 @@
+import 'package:dart_cli_full_banking_system/dart_cli_full_banking_system.dart' as dart_cli_full_banking_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_cli_full_banking_system.calculate()}!');
+}
---
+++ test/dart_cli_full_banking_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_cli_full_banking_system/dart_cli_full_banking_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
